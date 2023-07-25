d4861f68cb17a56b58bb460015780e0dd10c718f
Aldrich Heinz Alvarez <77214966+aldrichalvarez@users.noreply.github.com>
2023-07-25 18:58:30 +0800

    save progress

--- script.js
+++ script.js
@@ -1,34 +1,17 @@
-// hello, my name is Jason
-// 10
+function jasonsDesiredResult (sentence, limit) {
 
-// two inputs
-// string
-
-// - remove trailing spaces
-// - catch: if unable to fit for example 4 characters, cant split the word
-
-// return an error that says youc ant aprse
-
-// ['Hello, my', 'name is', 'Jason']
-
-function jasonsDesiredResult (sentence, number) {
-    
-    // if
-    // ["hello,", "my", "name", "is", "jason"]
-
-    // 1 split sentence
+    // this will split sentence into array of words
     const splittedSentence = sentence.split(" ")
 
-    // const cars = ["BMW", "Volvo", "Mini"];
-
-    // let text = "";
-    // for (let x of cars) {
-    //   text += x;
-    // }
-
+    // this is the array where we store the resulting lines
     let result = []
-    for (let x of splittedSentence) {
-        if (x.length > number) {
+
+    // this will loop through each word in the sentence
+    for (let word of splittedSentence) {
+
+        // this will check if the length of the current word is greater than the limit
+        if (word.length > number) {
+            // if its greater then throw error
             console.log("can't be parsed")
             break
         } else {
@@ -38,41 +21,13 @@ function jasonsDesiredResult (sentence, number) {
 
     }
 
-    // commits for every edit
-    // comments
-    // loom
 
-    // focus more on problem solving
-
-    // whats hard, whats wrong
-
-    // 10pm
-
-    // particles
-
-    // if (splittedSentence[])
-
-
-    // 2 check number of characters
-
-    // if number is > 10 then return error
-
-    // if number is < 10, add that word in the result
-
-
-
-
-
-
-
-
-
-
-    // 3
-
-    // 
 
     return result
 }
 
 console.log(jasonsDesiredResult("Hello, my name is Jason", 3))
+
+
+
+
